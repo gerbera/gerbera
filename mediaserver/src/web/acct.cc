ba576fc1f71d833a8004297cb2e2d737a78eb50f
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2005-05-29 23:38:13 +0000

    adapted GPL header

--- mediaserver/src/web/acct.cc
+++ mediaserver/src/web/acct.cc
@@ -1,4 +1,4 @@
-/*  browse.cc - this file is part of MediaTomb.
+/*  acct.cc - this file is part of MediaTomb.
                                                                                 
     Copyright (C) 2005 Gena Batyan <bgeradz@deadlock.dhs.org>,
                        Sergey Bostandzhyan <jin@deadlock.dhs.org>
