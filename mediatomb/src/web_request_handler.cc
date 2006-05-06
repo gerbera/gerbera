04a7b3364e5b8c58fdcc5e22d0c031aa058f6c25
Leonhard Wimmer <leo at mediatomb dot cc>
2006-05-06 02:00:40 +0000

    <time.h> instead of <sys/time.h>

--- mediatomb/src/web_request_handler.cc
+++ mediatomb/src/web_request_handler.cc
@@ -18,7 +18,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <sys/time.h>
+#include <time.h>
 #include "autoconfig.h"
 #include "config_manager.h"
 #include "mem_io_handler.h"
