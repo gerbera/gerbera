943d6bb31b36099f40871b8e360dc784da91561e
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2006-08-10 00:56:09 +0000

    reverted to old script

--- mediatomb/scripts/js/import.js
+++ mediatomb/scripts/js/import.js
@@ -1,17 +1,3 @@
-var Audio;
-var Audio_All;
-var Audio_Artists;
-var Audio_Fullname;
-var Audio_Albums;
-var Audio_Genres;
-var Audio_Date;
-
-var Video;
-
-var Photos;
-var Photos_All;
-var Photos_Date;
-
 function copyObject(obj)
 {
     var key;
@@ -69,20 +55,18 @@ function concatArrays(arr1, arr2)
     return arr;
 }
 
-function addContainer(parentID, title)
-{
-    var cont = new Object();
-    cont.objectType = OBJECT_TYPE_CONTAINER;
-    cont.parentID = parentID;
-    cont.title = title;
-    cont.upnp_class = 'object.container';
-    return addCdsObject(cont);
-}
-
-function addContainers(parentID, arr)
+function addContainerChain(parentID, arr)
 {
     for (var i = 0; i < arr.length; i++)
-        parentID = addContainer(parentID, arr[i]);
+    {
+        var title = arr[i];
+        var cont = new Object();
+        cont.objectType = OBJECT_TYPE_CONTAINER;
+        cont.parentID = parentID;
+        cont.title = title;
+        cont.upnp_class = 'object.container';
+        parentID = addCdsObject(cont);
+    }
     return parentID;
 }
 
@@ -144,102 +128,87 @@ function addAudio(obj)
         obj.meta[M_DESCRIPTION] = desc;
     }
 
-    var parentID;
-    if (! Audio)
-        Audio = addContainer(0, 'Audio');
-    
-    if (! Audio_All)
-        Audio_All = addContainer(Audio, 'All audio');
+    var chain = new Array('Audio', 'All audio');
     obj.title = title;
-    obj.parentID = Audio_All;
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    if (! Audio_Artists)
-        Audio_Artists = addContainer(Audio, 'Artists');
-    
-    obj.parentID = addContainers(Audio_Artists, [artist, 'All songs']);
+    chain = new Array('Audio', 'Artists', artist, 'All songs');
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    if (! Audio_Fullname)
-        Audio_Fullname = addContainer(Audio, 'All - full name');
+    chain = new Array('Audio', 'All - full name');
     obj.title = artist + ' - ' + album + ' - ' + title;
-    obj.parentID = Audio_Fullname;
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    obj.parentID = addContainers(Audio_Artists, [artist, 'All - full name']);
+    chain = new Array('Audio', 'Artists', artist, 'All - full name');
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    obj.parentID = addContainers(Audio_Artists, [artist, album]);
+    chain = new Array('Audio', 'Artists', artist, album);
     obj.title = title;
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    if (! Audio_Albums)
-        Audio_Albums = addContainer(Audio, 'Albums');
-    obj.parentID = addContainer(Audio_Albums, album);
+    chain = new Array('Audio', 'Albums', album);
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    if (! Audio_Genres)
-        Audio_Genres = addContainer(Audio, 'Genres');
-    obj.parentID = addContainer(Audio_Genres, genre);
+    chain = new Array('Audio', 'Genres', genre);
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
-    if (! Audio_Date)
-        Audio_Date = addContainer(Audio, 'Date');
-    obj.parentID = addContainer(Audio_Date, date);
+
+    chain = new Array('Audio', 'Date', date);
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
+
+    
 }
 
 // currently no video metadata supported
 function addVideo(obj)
 {
-    if (! Video)
-        Video = addContainer(0, 'Video');
-    obj.parentID = Video;
+    var chain = new Array('Video');
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 }
 
 // currently no image metadata supported
 function addImage(obj)
 {
-    if (! Photos)
-        Photos = addContainer(0, 'Photos');
-
-    if (! Photos_All)
-        Photos_All = addContainer(Photos, 'All Photos');
-    obj.parentID = Photos_All;
+    var chain = new Array('Photos', 'All Photos');
+    obj.parentID = addContainerChain("0", chain);
     addCdsObject(obj);
 
     var date = obj.meta[M_DATE];
     if (date)
     {
-        if (! Photos_Date)
-            Photos_Date = addContainer(Photos, 'Date');
-        obj.parentID = addContainer(Photos_Date, date);
+        chain = new Array('Photos', 'Date', date);
+        obj.parentID = addContainerChain("0", chain);
         addCdsObject(obj);
     }
 }
 
-function PROCESS_OBJECT(orig)
+var arr = orig.mimetype.split("/");
+var mime = arr[0];
+
+var obj = copyObject(orig);
+obj.refID = orig.id;
+
+if (mime == 'audio')
 {
-    var arr = orig.mimetype.split("/");
-    var mime = arr[0];
+    addAudio(obj);
+}
 
-    var obj = copyObject(orig);
-    obj.refID = orig.id;
+if (mime == 'video')
+{
+    addVideo(obj);
+}
 
-    if (mime == 'audio')
-    {
-        addAudio(obj);
-    }
-
-    if (mime == 'video')
-    {
-        addVideo(obj);
-    }
-
-    if (mime == 'image')
-    {
-        addImage(obj);
-    }
+if (mime == 'image')
+{
+    addImage(obj);
 }
 
