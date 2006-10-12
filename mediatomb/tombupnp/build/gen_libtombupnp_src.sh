2ec01b724e8d632f330175822f7b262361e34911
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2006-10-12 00:56:20 +0000

    major configure.ac cleanup; fix for distros that use --as-needed link flag.
    removed not needed win_dll.c from the tombupnp makefile

--- mediatomb/tombupnp/build/gen_libtombupnp_src.sh
+++ mediatomb/tombupnp/build/gen_libtombupnp_src.sh
@@ -1,6 +1,6 @@
 echo 'libtombupnp_a_SOURCES = \'
 
-find ../ | egrep '\.(c|h)$' | sed 's/$/ \\/' | sort 
+find ../ | egrep '\.(c|h)$' | egrep -v 'win_dll\.c' | sed 's/$/ \\/' | sort 
 
 echo "dummy.h"
 
