2de4932419218bfa39693c64a576ab8272b9312b
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2006-11-03 00:18:13 +0000

    checking in new files without functionality - Leo is running the header comment script

---
+++ mediatomb/src/autoscan_directory.cc
@@ -0,0 +1,8 @@
+/*MT*/
+
+#include "autoscan_directory.h"
+
+AutoscanDirectory::AutoscanDirectory(zmm::String location, scan_mode_t mode,
+                      scan_level_t level, bool recursive,
+                      unsigned int interval=0);
+
