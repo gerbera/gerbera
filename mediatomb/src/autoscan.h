e7e5344fb90895d77a100fb5aee818456e75821b
Leonhard Wimmer <leo at mediatomb dot cc>
2006-11-23 00:37:23 +0000

    Id-Tag corrected

--- mediatomb/src/autoscan.h
+++ mediatomb/src/autoscan.h
@@ -24,7 +24,7 @@
     along with MediaTomb; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     
-    $ID: autoscan_directory.h 1126 2006-11-03 01:53:53Z jin $
+    $Id$
 */
 
 /// \file autoscan.h
