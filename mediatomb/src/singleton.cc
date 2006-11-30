0892625414f5b743322e005a690bef19a6191e89
Leonhard Wimmer <leo at mediatomb dot cc>
2006-11-30 19:40:21 +0000

    added svn:keywords Id to some new files

--- mediatomb/src/singleton.cc
+++ mediatomb/src/singleton.cc
@@ -24,7 +24,7 @@
     along with MediaTomb; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     
-    $Id: object.h 1123 2006-11-03 01:03:30Z leo $
+    $Id$
 */
 
 /// \file singleton.cc
