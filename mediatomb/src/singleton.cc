e122382665dd102b31a5eb75780c739d0342ec05
Leonhard Wimmer <leo at mediatomb dot cc>
2006-11-30 02:34:39 +0000

    forgot to check in singleton.(h|cc)

---
+++ mediatomb/src/singleton.cc
@@ -0,0 +1,80 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.org/
+    
+    singleton.cc - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>
+    Copyright (C) 2006 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>,
+                       Leonhard Wimmer <leo@mediatomb.org>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    
+    $Id: object.h 1123 2006-11-03 01:03:30Z leo $
+*/
+
+/// \file singleton.cc
+
+#ifdef HAVE_CONFIG_H
+    #include "autoconfig.h"
+#endif
+
+#include "singleton.h"
+
+using namespace zmm;
+
+#define SINGLETON_MANAGEMENT_INITIAL_CAPACITY 30
+
+Ref<SingletonManagement> SingletonManagement::instance = nil;
+Ref<Mutex> SingletonManagement::mutex = Ref<Mutex>(new Mutex());
+
+Ref<SingletonManagement> SingletonManagement::getInstance()
+{
+    if (instance == nil)
+    {
+        AUTOLOCK(mutex);
+        if (instance == nil) // check again, because there is a very small chance
+                             // that 2 threads tried to lock() concurrently
+        {
+            instance = zmm::Ref<SingletonManagement>(new SingletonManagement());
+        }
+    }
+    return instance;
+}
+
+SingletonManagement::SingletonManagement() : Object()
+{
+    singletonStack = Ref<ObjectStack<Singleton<Object> > >(new ObjectStack<Singleton<Object> >(SINGLETON_MANAGEMENT_INITIAL_CAPACITY));
+}
+
+void SingletonManagement::registerSingleton(Ref<Singleton<Object> > object)
+{
+    singletonStack->push(object);
+}
+
+void SingletonManagement::shutdown()
+{
+    log_debug("start (%d objects)\n", singletonStack->size());
+    AUTOLOCK(mutex);
+    Ref<Singleton<Object> > object;
+    while((object = singletonStack->pop()) != nil)
+    {
+        object->shutdown();
+    }
+    log_debug("end\n");
+}
+
---
+++ mediatomb/src/singleton.h
@@ -0,0 +1,98 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.org/
+    
+    singleton.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>
+    Copyright (C) 2006 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>,
+                       Leonhard Wimmer <leo@mediatomb.org>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    
+    $Id: object.h 1123 2006-11-03 01:03:30Z leo $
+*/
+
+/// \file singleton.h
+
+#ifndef __SINGLETON_H__
+#define __SINGLETON_H__
+
+#include "sync.h"
+#include "zmmf/zmmf.h"
+
+template <class T> class Singleton;
+
+class SingletonManagement : public zmm::Object
+{
+public:
+    static zmm::Ref<SingletonManagement> getInstance();
+    SingletonManagement();
+    
+    void registerSingleton(zmm::Ref<Singleton<zmm::Object> > object);
+    void shutdown();
+    
+protected:
+    static zmm::Ref<SingletonManagement> instance;
+    static zmm::Ref<Mutex> mutex;
+    
+    zmm::Ref<zmm::ObjectStack<Singleton<zmm::Object> > > singletonStack;
+};
+
+template <class T>
+class Singleton : public zmm::Object
+{
+public:
+    
+    Singleton()
+    {
+        SingletonManagement::getInstance()->registerSingleton(zmm::Ref<Singleton<Object> >((Singleton<Object> *)this));
+    }
+    
+    virtual ~Singleton() { };
+    
+    static zmm::Ref<T> getInstance()
+    {
+        if (instance == nil)
+        {
+            AUTOLOCK(mutex);
+            if (instance == nil) // check again, because there is a very small chance
+                                 // that 2 threads tried to lock() concurrently
+            {
+                instance = zmm::Ref<T>(new T());
+                instance->init();
+            }
+        }
+        return instance;
+    }
+    
+protected:
+    virtual void init() { };
+    virtual void shutdown() { };
+    static zmm::Ref<T> instance;
+    static zmm::Ref<Mutex> mutex;
+    
+    friend class SingletonManagement;
+};
+
+#define SINGLETON_NEED_RECURSIVE_MUTEX(klass) template <> zmm::Ref<Mutex> Singleton<klass>::mutex = zmm::Ref<Mutex>(new Mutex(true))
+
+template <class T> zmm::Ref<Mutex> Singleton<T>::mutex = zmm::Ref<Mutex>(new Mutex());
+template <class T> zmm::Ref<T> Singleton<T>::instance = nil;
+
+#endif // __SINGLETON_H__
+
