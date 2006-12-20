b26343aa07aa0bb650cc2072f73fcfa8a0ce065d
Leonhard Wimmer <leo at mediatomb dot cc>
2006-12-20 20:30:50 +0000

    changed headers to GPL=2 instead of GPL>=2

--- mediatomb/scripts/update-comment-header.pl
+++ mediatomb/scripts/update-comment-header.pl
@@ -21,9 +21,8 @@ foreach (@ARGV)
                        Leonhard Wimmer <leo@mediatomb.org>
     
     MediaTomb is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
     
     MediaTomb is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -31,7 +30,7 @@ foreach (@ARGV)
     GNU General Public License for more details.
     
     You should have received a copy of the GNU General Public License
-    along with MediaTomb; if not, write to the Free Software
+    version 2 along with MediaTomb; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     
     $Id$';
@@ -85,11 +84,11 @@ foreach (@ARGV)
         print "#*MT*...";
         $data =~ s/#\*MT\*.*?#\*MT\*/$new_header_number/s;
     }
-    elsif ($data =~ m|/\*.*this file is part of MediaTomb\.|)
-    {
-        print "old header";
-        $data =~ s|/\*.*this file is part of MediaTomb\..*?\*/|$new_header_c|s;
-    }
+#    elsif ($data =~ m|/\*.*this file is part of MediaTomb\.|)
+#    {
+#        print "old header";
+#        $data =~ s|/\*.*this file is part of MediaTomb\..*?\*/|$new_header_c|s;
+#    }
     else
     {
         $nothing = 1;
--- mediatomb/src/content_manager.cc
+++ mediatomb/src/content_manager.cc
@@ -11,9 +11,8 @@
                        Leonhard Wimmer <leo@mediatomb.org>
     
     MediaTomb is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
     
     MediaTomb is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +20,7 @@
     GNU General Public License for more details.
     
     You should have received a copy of the GNU General Public License
-    along with MediaTomb; if not, write to the Free Software
+    version 2 along with MediaTomb; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     
     $Id$
@@ -550,7 +549,7 @@ void ContentManager::_rescanDirectory(int containerID, int scanID, scan_mode_t s
                 // in this case we will invalidate the autoscan entry
                 if (adir->getScanID() == INVALID_SCAN_ID)
                     return;
-
+                
                 // add directory, recursive, async, hidden flag, low priority
                 addFile(path, true, true, adir->getHidden(), true);
             }
