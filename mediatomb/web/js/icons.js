241bccab653a501a036a287dbc866f21ab61bf42
Leonhard Wimmer <leo at mediatomb dot cc>
2006-12-23 21:14:04 +0000

    forgot to check new icons.js

---
+++ mediatomb/web/js/icons.js
@@ -0,0 +1,60 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.org/
+    
+    icons.js - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>
+    Copyright (C) 2006 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>,
+                       Leonhard Wimmer <leo@mediatomb.org>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    
+    $Id$
+*/
+
+/* icons for items.js */
+
+var iconPath = '/icons/';
+var iconNewItem = iconPath + 'document-new.png';
+var iconAdd = iconPath + 'stock-add.png';
+var iconEdit = iconPath + 'stock_edit.png';
+var iconRefresh = iconPath + 'view-refresh.png';
+var iconRemoveThis = iconPath + 'remove_this.png';
+var iconRemoveAll = iconPath + 'remove_all.png';
+var iconFirst = iconPath + 'go-first.png';
+var iconPrevious = iconPath + 'go-previous.png';
+var iconNext = iconPath + 'go-next.png';
+var iconLast = iconPath + 'go-last.png';
+var iconAddAutoscan = iconPath + 'add_as_autoscan.png';
+var iconRemoveAutoscan = iconPath + 'remove_autoscan.png';
+
+/* icons for tree.js */
+
+var treeImagePath = '/icons/nanotree/images/';
+var iconTreeClosed = treeImagePath + 'folder_closed.png';
+var iconTreeOpen = treeImagePath + 'folder_open.png';
+var iconTreeAutoscanClosed = treeImagePath + 'autoscan_folder_closed.png';
+var iconTreeAutoscanOpen = treeImagePath + 'autoscan_folder_open.png';
+var iconTreeAutoscanConfigOpen = treeImagePath + 'autoscan_folder_open.png';
+var iconTreeAutoscanConfigClosed = treeImagePath + 'autoscan_folder_open.png';
+
+
+/* FIXME - should be replaced by icons w/o line on the left */
+var iconContainer = treeImagePath + 'folder_open.png';
+var iconContainerAutoscan = treeImagePath + 'autoscan_folder_open.png';
+var iconContainerAutoscanConfig = treeImagePath + 'autoscan_folder_open.png';
+
