b63c1168d46239e7a9e50bb2bc9ec6b0eaceff1c
Leonhard Wimmer <leo at mediatomb dot cc>
2006-12-24 03:51:35 +0000

    added GPL header to .css

--- mediatomb/scripts/update-comment-header.sh
+++ mediatomb/scripts/update-comment-header.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find web src tombupnp |egrep '\.(cc?|h|html?|js)$' | xargs perl scripts/update-comment-header.pl
+find web src tombupnp |egrep '\.(cc?|h|html?|js|css)$' | xargs perl scripts/update-comment-header.pl
 perl scripts/update-comment-header.pl configure.ac
