5b1d7524d055cda2be8da1f7eb6512171c9da461
Leonhard Wimmer <leo at mediatomb dot cc>
2006-12-24 04:08:00 +0000

    added some $Id$ tags

--- mediatomb/web/js/autoscan.js
+++ mediatomb/web/js/autoscan.js
@@ -1,3 +1,31 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.org/
+    
+    autoscan.js - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>
+    Copyright (C) 2006 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey Bostandzhyan <jin@mediatomb.org>,
+                       Leonhard Wimmer <leo@mediatomb.org>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    
+    $Id$
+*/
+
 var autoscanAddId;
 var autoscanFromFs;
 
