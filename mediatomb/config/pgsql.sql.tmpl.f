14d86a2b4bf0e6e90bd1d91c4408d8b6e7c446bd
Leonhard Wimmer <leo at mediatomb dot cc>
2007-01-26 20:04:01 +0000

    preparing for postgres

---
+++ mediatomb/config/pgsql.sql.tmpl.f
@@ -0,0 +1,4 @@
+#endif // __PGSQL_CREATE_SQL_H__
+
+#endif // HAVE_PGSQL
+#endif // AUTO_CREATE_DATABASE
---
+++ mediatomb/config/pgsql.sql.tmpl.h
@@ -0,0 +1,37 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.org/
+    
+    pgsql_create_sql.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.org>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.org>
+    
+    Copyright (C) 2006-2007 Gena Batyan <bgeradz@mediatomb.org>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.org>,
+                            Leonhard Wimmer <leo@mediatomb.org>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    
+    $Id$
+*/
+
+/// \file pgsql_create_sql.h
+
+#ifdef AUTO_CREATE_DATABASE
+#ifdef HAVE_PGSQL
+
+#ifndef __PGSQL_CREATE_SQL_H__
+#define __PGSQL_CREATE_SQL_H__
+
