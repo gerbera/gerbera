7498c8c827834c8148ddb9e472bd85b7947288b4
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2007-02-23 20:01:19 +0000

    abstract layout class

---
+++ mediatomb/src/layout/layout.h
@@ -0,0 +1,45 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    layout.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2007 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+    $Id: scripting.h 1124 2007-02-17 21:49:15Z lww $
+*/
+
+/// \file layout.h
+
+#ifndef __LAYOUT_H__
+#define __LAYOUT_H__
+
+#include "cds_objects.h"
+
+class Layout : public zmm::Object
+{
+public:
+	virtual void init() = 0;
+	virtual void processCdsObject(zmm::Ref<CdsObject> obj) = 0;	
+};
+
+#endif // __SCRIPTING_H__
+
