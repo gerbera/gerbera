a54572ace9c06a7851861f08ee1a1c1f2288467b
Leonhard Wimmer <leo at mediatomb dot cc>
2007-03-09 00:10:15 +0000

    gen_readme script now generates txt versions too

--- mediatomb/scripts/gen_readme_html.sh
+++ mediatomb/scripts/gen_readme_html.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 if [ -e readme.lyx ]; then
+    rm -f readme.xml
     lyx -e docbook-xml readme.lyx
     xmlto xhtml-nochunks -m sections.xsl readme.xml
-    perl ../scripts/readme_xhtml_div_extract.pl < readme.html > readme_ruby.html
+    perl ../scripts/readme_xhtml_div_extract.pl < readme.html > readme_part.html
+    xmlto txt -m sections.xsl readme.xml
+    mv readme.txt README.UTF_8
+    perl ../scripts/readme_utf-8_acsii_convert.pl < README.UTF_8 > README
 else
     echo readme.lyx not found
 fi
