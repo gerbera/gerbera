6e52c1b5ce2a62fd66c511641684493d2eb832b2
Leonhard Wimmer <leo at mediatomb dot cc>
2007-03-25 21:50:22 +0000

    gen_readme now takes care of scripting too

--- mediatomb/scripts/gen_readme.sh
+++ mediatomb/scripts/gen_readme.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
-if [ -e readme.lyx ]; then
-    rm -f readme.xml && \
-    lyx -e docbook-xml readme.lyx && \
-    xmlto xhtml-nochunks -m sections.xsl readme.xml && \
-    perl ../scripts/readme_xhtml_div_extract.pl < readme.html > readme_part.html && \
-    xmlto txt -m sections.xsl readme.xml && \
-    mv readme.txt README.UTF_8 && \
-    perl ../scripts/readme_utf-8_acsii_convert.pl < README.UTF_8 > README && \
+
+gen_readme () {
+
+if [ -e $FILENAME.lyx ]; then
+    rm -f $FILENAME.xml && \
+    lyx -e docbook-xml $FILENAME.lyx && \
+    xmlto xhtml-nochunks -m sections.xsl $FILENAME.xml && \
+    perl ../scripts/readme_xhtml_div_extract.pl < $FILENAME.html > $FILENAME_part.html && \
+    xmlto txt -m sections.xsl $FILENAME.xml && \
+    mv $FILENAME.txt $FILENAME_UTF_8 && \
+    perl ../scripts/readme_utf-8_acsii_convert.pl < $FILENAME_UTF_8 > $FILENAME_ASCII && \
     echo finished successfully
 else
-    echo readme.lyx not found
+    echo $FILENAME.lyx not found
 fi
+
+}
+
+FILENAME=readme
+FILENAME_ASCII=README
+FILENAME_UTF_8=$FILENAME_BIG.UTF_8
+gen_readme
+
+FILENAME=scripting
+FILENAME_ASCII=scripting.txt
+FILENAME_UTF_8=scripting_utf8.txt
+gen_readme
 
