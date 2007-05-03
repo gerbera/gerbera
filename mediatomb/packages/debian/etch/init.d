226ebcb4594d1077ea82e5f02e8184ef2c6d50b2
Leonhard Wimmer <leo at mediatomb dot cc>
2007-05-03 13:54:09 +0000

    checkin of files needed to build debian and ubuntu packages

---
+++ mediatomb/packages/debian/etch/init.d
@@ -0,0 +1,117 @@
+#! /bin/sh 
+#
+# MediaTomb initscript
+#
+# Original Author: Tor Krill <tor@excito.com>.
+# Modified by:     Leonhard Wimmer <leo@mediatomb.cc>
+#
+#
+
+### BEGIN INIT INFO
+# Provides:          mediatomb
+# Required-Start:    $all
+# Required-Stop:     $all
+# Should-Start:      $all
+# Should-Stop:       $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: upnp media server
+### END INIT INFO
+
+
+set -e
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DESC="upnp media server"
+NAME=mediatomb
+DAEMON=/usr/bin/$NAME
+PIDFILE=/var/run/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+USER=mediatomb
+GROUP=mediatomb
+LOGFILE=/var/log/mediatomb
+
+# Read config file if it is present.
+if [ -r /etc/default/$NAME ]
+then
+	. /etc/default/$NAME
+fi
+
+# if NO_START is set, exit gracefully
+[ "$NO_START" != "" ] && exit 0
+
+D_ARGS="-c /etc/mediatomb/config.xml -d -u $USER -g $GROUP -P $PIDFILE -l $LOGFILE"
+
+# Gracefully exit if the package has been removed.
+test -x $DAEMON || exit 0
+
+#
+#       Function that starts the daemon/service.
+#
+d_start() {
+	touch $PIDFILE
+	chown $USER:$GROUP $PIDFILE
+	touch $LOGFILE
+	chown $USER:$GROUP $LOGFILE
+        start-stop-daemon --start --quiet --pidfile $PIDFILE \
+                --exec $DAEMON -- $D_ARGS $DAEMON_OPTS
+}
+
+#
+#       Function that stops the daemon/service.
+#
+d_stop() {
+        start-stop-daemon --stop --signal 2 --retry 5 --quiet --pidfile $PIDFILE \
+                --name $NAME
+        rm $PIDFILE
+}
+
+#
+#       Function that sends a SIGHUP to the daemon/service.
+#
+d_reload() {
+        start-stop-daemon --stop --quiet --pidfile $PIDFILE \
+                --name $NAME --signal 1
+}
+
+case "$1" in
+  start)
+        echo -n "Starting $DESC: $NAME"
+        if [ "$INTERFACE" != "" ] ; then
+            # try to add the multicast route
+            /sbin/route add -net 239.0.0.0 netmask 255.0.0.0 $INTERFACE >/dev/null 2>&1 || true
+        fi
+        d_start
+        echo "."
+        ;;
+  stop)
+        echo -n "Stopping $DESC: $NAME"
+        d_stop
+        echo "."
+        ;;
+  reload|force-reload)
+        echo -n "Reloading $DESC configuration..."
+        d_reload
+        echo "done."
+  	;;
+  restart)
+        #
+        #       If the "reload" option is implemented, move the "force-reload"
+        #       option to the "reload" entry above. If not, "force-reload" is
+        #       just the same as "restart".
+        #
+        echo -n "Restarting $DESC: $NAME"
+        d_stop
+        sleep 1
+        d_start
+        echo "."
+        ;;
+  *)
+        # echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
+        echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
+
