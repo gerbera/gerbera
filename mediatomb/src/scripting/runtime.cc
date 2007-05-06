282bc65adcaaa39df089bb782426ce6b894381db
Leonhard Wimmer <leo at mediatomb dot cc>
2007-05-06 12:35:55 +0000

    clean up completely messed up scripting headers

--- mediatomb/src/scripting/runtime.cc
+++ mediatomb/src/scripting/runtime.cc
@@ -23,9 +23,11 @@
     You should have received a copy of the GNU General Public License
     version 2 along with MediaTomb; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-
+    
     $Id$
 */
+
+/// \file runtime.cc
 
 #ifdef HAVE_CONFIG_H
     #include "autoconfig.h"
--- mediatomb/src/scripting/script.h
+++ mediatomb/src/scripting/script.h
@@ -1,23 +1,30 @@
-/*  scripting.h - this file is part of MediaTomb.
-                                                                                
-    Copyright (C) 2005 Gena Batyan <bgeradz@deadlock.dhs.org>,
-                       Sergey Bostandzhyan <jin@deadlock.dhs.org>
-                                                                                
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    script.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2007 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
     MediaTomb is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-                                                                                
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
     MediaTomb is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-                                                                                
+    
     You should have received a copy of the GNU General Public License
-    along with MediaTomb; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    $Id $
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+    $Id$
 */
 
 /// \file script.h
@@ -79,6 +86,4 @@ public:
 };
 
 #endif // __SCRIPTING_SCRIPT_H__
-
-
 
