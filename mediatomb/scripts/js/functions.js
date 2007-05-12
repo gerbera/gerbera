0b56a163b112deb1393a42408cb9096eaf2a1304
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2007-05-12 22:57:00 +0000

    common functions (the ones that are use by both, the playlist and the import scripts) are now defined in functions.js

---
+++ mediatomb/scripts/js/functions.js
@@ -0,0 +1,26 @@
+function escapeSlash(name)
+{   
+    name = name.replace(/\\/g, "\\\\");
+    name = name.replace(/\//g, "\\/");
+    return name;
+}   
+
+function createContainerChain(arr)
+{   
+    var path = '';
+    for (var i = 0; i < arr.length; i++)
+    {
+        path = path + '/' + escapeSlash(arr[i]);
+    }
+    return path;
+}
+
+function getYear(date)
+{
+    var matches = date.match(/^([0-9]{4})-/);
+    if (matches)
+        return matches[1];
+    else
+        return date;
+}   
+
--- mediatomb/scripts/js/playlists.js
+++ mediatomb/scripts/js/playlists.js
@@ -1,20 +1,3 @@
-function escapeSlash(name)
-{
-    name = name.replace(/\\/g, "\\\\");
-    name = name.replace(/\//g, "\\/");
-    return name;
-}
-
-function createContainerChain(arr)
-{
-    var path = '';
-    for (var i = 0; i < arr.length; i++)
-    {
-        path = path + '/' + escapeSlash(arr[i]);
-    }
-    return path;
-}
-
 function addPlaylistItem(location, playlistChain, duration, title)
 {
     if (location.match(/^http/))
