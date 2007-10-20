434490f180888791504280af4d88c50b40585f51
Leonhard Wimmer <leo at mediatomb dot cc>
2007-10-20 13:42:06 +0000

    adding new Executor class

---
+++ mediatomb/src/executor.h
@@ -0,0 +1,44 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    executor.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2007 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+    $Id$
+*/
+
+/// \file executor.h
+
+#ifndef __EXECUTOR_H__
+#define __EXECUTOR_H__
+
+#include "zmmf/zmmf.h"
+
+class Executor : public zmm::Object
+{
+public:
+    virtual bool isAlive() = 0;
+    virtual bool kill() = 0;
+};
+
+#endif // __EXECUTOR_H__
