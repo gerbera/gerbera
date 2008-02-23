37e7095cbf8e1838dd629d7290bab3a19a062cc0
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2008-02-23 21:11:58 +0000

    reverted the change, indentation after reinserting the udn tag messed up all comments

--- mediatomb/src/config_manager.cc
+++ mediatomb/src/config_manager.cc
@@ -1709,10 +1709,7 @@ void ConfigManager::prepare_udn()
     }
     
     if (need_to_save)
-    {
-        root->indent();
         save();
-    }
 }
 
 void ConfigManager::prepare_path(String xpath, bool needDir, bool existenceUnneeded)
