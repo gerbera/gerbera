fddeabe36ca5c9c40259cf54021750e73fa20475
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2008-10-12 18:40:46 +0000

    added missing semicolons

--- mediatomb/scripts/js/import-dvd.js
+++ mediatomb/scripts/js/import-dvd.js
@@ -1,15 +1,35 @@
-print("Processing object: " + dvd.title + " at " + dvd.location);
+// Default MediaTomb dvd import script.
+// see MediaTomb scripting documentation for more information
 
+/*MT_F*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    import-dvd.js - this file is part of MediaTomb.
+    
+    Copyright (C) 2006-2008 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    This file is free software; the copyright owners give unlimited permission
+    to copy and/or redistribute it; with or without modifications, as long as
+    this notice is preserved.
+    
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    
+    $Id$
+*/
+
+var title_count = dvd.aux[DVD].titles.length;
 var title = dvd.title;
 var index = title.lastIndexOf('.');
-
-// we got the ISO here but our virtual items should have the video class
-dvd.upnpclass = UPNP_CLASS_ITEM_VIDEO;
-
 if (index > 1)
     title = title.substring(0, index);
 
-var title_count = dvd.aux[DVD].titles.length;
+// we got the ISO here but our virtual items should have the video class
+dvd.upnpclass = UPNP_CLASS_ITEM_VIDEO;
 
 for (var t = 0; t < title_count; t++)
 {
@@ -20,28 +40,23 @@ for (var t = 0; t < title_count; t++)
     else
         title_name = title_name + ' ' + (t + 1);
 
-    var audio_track_count = dvd.aux[DVD].titles[t].audio_tracks.length
-
-    var chapter_count = dvd.aux[DVD].titles[t].chapters.length
+    var audio_track_count = dvd.aux[DVD].titles[t].audio_tracks.length;
+    var chapter_count = dvd.aux[DVD].titles[t].chapters.length;
 
     for (var a = 0; a < audio_track_count; a++)
     {
         var chain;
-
         var audio_name = ' - Audio Track ' + (a + 1);
-
-        var audio_language = dvd.aux[DVD].titles[t].audio_tracks[a].language
-       
+        var audio_language = dvd.aux[DVD].titles[t].audio_tracks[a].language;
         var audio_format = dvd.aux[DVD].titles[t].audio_tracks[a].format;
         if (audio_format != '')
         {
             if (audio_language != '')
                 audio_name = audio_name + ' - ' + audio_language;
 
-            chain = new Array('Video', 'DVD', 'Audio Formats', audio_format,
-                    title_name + audio_name);
+            chain = new Array('Video', 'DVD', title, 'Audio Formats', 
+                              audio_format, title_name + audio_name);
 
-            print("Chain used: " + createContainerChain(chain));
             for (var c = 0; c < chapter_count; c++) 
             {
                 if (c < 9)
@@ -55,13 +70,13 @@ for (var t = 0; t < title_count; t++)
 
         if (audio_language != '')
         {
-            chain = new Array('Video', 'DVD', 'Languages', audio_language);
+            chain = new Array('Video', 'DVD', title, 'Languages', 
+                              audio_language);
             if (audio_format != '')
                 chain.push(title_name + audio_name + ' - ' + audio_format);
             else 
                 chain.push(title_name + audio_name);
 
-            print("Chain used: " + createContainerChain(chain));
             for (var c = 0; c < chapter_count; c++)
             {
                 if (c < 9)
@@ -73,7 +88,7 @@ for (var t = 0; t < title_count; t++)
             } 
         }
 
-        chain = new Array('Video', 'DVD', 'Titles');
+        chain = new Array('Video', 'DVD', title, 'Titles');
 
         var titles = title_name + ' - Audio Track ' + (a + 1);
 
@@ -85,7 +100,6 @@ for (var t = 0; t < title_count; t++)
 
         chain.push(titles);
         
-        print("Chain used: " + createContainerChain(chain));
         for (var c = 0; c < chapter_count; c++)
         {
             if (c < 9)
