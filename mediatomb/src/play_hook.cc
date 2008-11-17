13d941dfcee8cfc23bbf0aaaac70480f137dba64
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2008-11-17 19:49:46 +0000

    and as always... forgot to commit new files :)

---
+++ mediatomb/src/play_hook.cc
@@ -0,0 +1,61 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    play_hook.cc - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2008 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+*/
+
+/// \file play_hook.cc
+/// \brief Definition of the PlayHook class.
+//
+// \todo this should be solved via an observer model 
+
+#ifdef HAVE_CONFIG_H
+    #include "autoconfig.h"
+#endif
+
+#include "play_hook.h"
+#include "config_manager.h"
+#include "content_manager.h"
+
+using namespace zmm;
+
+SINGLETON_MUTEX(PlayHook, false);
+
+void PlayHook::trigger(zmm::Ref<CdsObject> obj)
+{
+    Ref<ConfigManager> cfg = ConfigManager::getInstance();
+
+    if (cfg->getBoolOption(CFG_SERVER_EXTOPTS_MARK_PLAYED_ITEMS_ENABLED))
+    {
+        obj->setFlag(OBJECT_FLAG_PLAYED);
+
+        bool supress = cfg->getBoolOption(CFG_SERVER_EXTOPTS_MARK_PLAYED_ITEMS_SUPRESS_CDS_UPDATES);
+
+        Ref<ContentManager> cm = ContentManager::getInstance();
+        cm->updateObject(obj, !supress);
+    }
+}
+
+
---
+++ mediatomb/src/play_hook.h
@@ -0,0 +1,48 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    play_hook.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2008 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+*/
+
+/// \file play_hook.h
+/// \brief Definition of the PlayHook class.
+//
+// \todo this should be solved via an observer model 
+
+#ifndef __PLAY_HOOK_H__
+#define __PLAY_HOOK_H__
+
+#include "singleton.h"
+#include "common.h"
+#include "cds_objects.h"
+
+class PlayHook : public Singleton<PlayHook>
+{
+public:
+    void trigger(zmm::Ref<CdsObject> obj);
+};
+
+#endif//__PLAY_HOOK_H__
+
