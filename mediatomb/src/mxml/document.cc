dea316955ed0df212bb552786e4c13ffa36045c3
Leonhard Wimmer <leo at mediatomb dot cc>
2010-02-04 15:09:17 +0000

    sh*t! forgot too add document.(h|cc)... :>

---
+++ mediatomb/src/mxml/document.cc
@@ -0,0 +1,76 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    comment.cc - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2009 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+    $Id$
+*/
+
+/// \file document.cc
+
+#ifdef HAVE_CONFIG_H
+    #include "autoconfig.h"
+#endif
+
+#include "mxml.h"
+#include "document.h"
+
+using namespace zmm;
+using namespace mxml;
+
+Document::Document() : Node()
+{
+    type = mxml_node_document;
+    root = nil;
+}
+
+void Document::setRoot(Ref<Element> root)
+{
+    this->root = root;
+    appendChild(RefCast(root, Node));
+}
+
+Ref<Element> Document::getRoot()
+{
+    return root;
+}
+
+void Document::appendChild(Ref<Node> child)
+{
+    if(children == nil)
+        children = Ref<Array<Node> >(new Array<Node>());
+    children->append(child);
+}
+
+void Document::print_internal(Ref<StringBuffer> buf, int indent)
+{
+    if (children != nil && children->size())
+    {
+        for(int i = 0; i < children->size(); i++)
+        {
+            children->get(i)->print_internal(buf, indent + 1);
+        }
+    }
+}
+
---
+++ mediatomb/src/mxml/document.h
@@ -0,0 +1,62 @@
+/*MT*
+    
+    MediaTomb - http://www.mediatomb.cc/
+    
+    node.h - this file is part of MediaTomb.
+    
+    Copyright (C) 2005 Gena Batyan <bgeradz@mediatomb.cc>,
+                       Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>
+    
+    Copyright (C) 2006-2009 Gena Batyan <bgeradz@mediatomb.cc>,
+                            Sergey 'Jin' Bostandzhyan <jin@mediatomb.cc>,
+                            Leonhard Wimmer <leo@mediatomb.cc>
+    
+    MediaTomb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+    
+    MediaTomb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    version 2 along with MediaTomb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+    
+    $Id$
+*/
+
+/// \file document.h
+
+#ifndef __MXML_DOCUMENT_H__
+#define __MXML_DOCUMENT_H__
+
+#include "zmmf/zmmf.h"
+
+#include "mxml.h"
+
+namespace mxml
+{
+    
+class Element;
+
+class Document : public Node
+{
+protected:
+    zmm::Ref<Element> root;
+    
+public:
+    Document();
+    
+    zmm::Ref<Element> getRoot();
+    void setRoot(zmm::Ref<Element> root);
+    void appendChild(zmm::Ref<Node> child);
+    
+protected:
+    virtual void print_internal(zmm::Ref<zmm::StringBuffer> buf, int indent);
+};
+
+} // namespace
+
+#endif // __MXML_DOCUMENT_H__
