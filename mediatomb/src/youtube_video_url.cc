2abeff245aaf63e8ccc69fc9fab9e37931f8bfb4
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2010-04-03 20:22:31 +0000

    Fix for some compilers that complain about "\&"

--- mediatomb/src/youtube_video_url.cc
+++ mediatomb/src/youtube_video_url.cc
@@ -49,7 +49,7 @@ using namespace zmm;
 #define YOUTUBE_URL_WATCH           "http://www.youtube.com/watch?v="
 #define YOUTUBE_URL_GET             "http://www.youtube.com/get_video?" 
 #define YOUTUBE_URL_PARAM_VIDEO_ID  "video_id"
-#define YOUTUBE_URL_PARAM_T_REGEXP  ".*\&t=([^\&]+)\&"
+#define YOUTUBE_URL_PARAM_T_REGEXP  ".*&t=([^&]+)&"
 #define YOUTUBE_URL_PARAM_T         "t"
 #define YOUTUBE_IS_HD_AVAILABLE_REGEXP  "IS_HD_AVAILABLE[^:]*: *([^,]*)"
 YouTubeVideoURL::YouTubeVideoURL()
