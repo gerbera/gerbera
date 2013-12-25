f1902a278151ef1c0dd4c3c18b3f81a433c6ae21
Sergey 'Jin' Bostandzhyan <jin at mediatomb dot cc>
2013-12-26 00:22:43 +0100

    Make use of arc4random on *BSD/OS X
    Integrated patch by Brad Smith:
    https://sourceforge.net/p/mediatomb/patches/44/
    
    The attached patch adds support for utilizing the arc4random() function on *BSD OS's and OS X to provide a better seed to srand(). Pushing upstream from the OpenBSD ports tree.

--- tombupnp/threadutil/src/ThreadPool.c
+++ tombupnp/threadutil/src/ThreadPool.c
@@ -372,8 +372,8 @@ tp->stats.totalJobsLQ++; tp->stats.totalTimeLQ += diff; break; default:
     gettimeofday( &t, NULL );
 #if defined(WIN32)
     srand( ( unsigned int )(t.tv_usec/1000) + (unsigned int)ithread_get_current_thread_id(  ).p );
-#elif defined(__FreeBSD__) || defined (__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__)
-    srand( ( unsigned int )(t.tv_usec/1000) + (unsigned int)ithread_get_current_thread_id(  ) );
+#elif defined(HAVE_ARC4RANDOM)
+    srand( arc4random() );
 #else
     srand( ( unsigned int )(t.tv_usec/1000) + ithread_get_current_thread_id(  ) );
 #endif
