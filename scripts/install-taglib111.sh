e4285cfed018d1f6a83f491ab30e40a1d20978d4
Ian Whyman <thev00d00@gentoo.org>
2017-02-03 14:33:28 +0000

    CI: taglib match suffixed clang

--- scripts/install-taglib111.sh
+++ scripts/install-taglib111.sh
@@ -4,7 +4,7 @@ wget http://taglib.github.io/releases/taglib-1.11.1.tar.gz
 tar -xzvf taglib-1.11.1.tar.gz
 mkdir taglib-build
 cd taglib-build
-if [ "$CXX" == "clang++" ]; then
+if [[ $CXX == clang++* ]]; then
 	cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" ../taglib-1.11.1
 else
 	cmake ../taglib-1.11.1
