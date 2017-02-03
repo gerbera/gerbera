bd96601ba42ae062330c23fe9633c995bb7836b2
Ian Whyman <thev00d00@gentoo.org>
2017-02-03 13:39:29 +0000

    CI: build taglib with libc++ if clang

--- scripts/install-taglib111.sh
+++ scripts/install-taglib111.sh
@@ -4,5 +4,9 @@ wget http://taglib.github.io/releases/taglib-1.11.1.tar.gz
 tar -xzvf taglib-1.11.1.tar.gz
 mkdir taglib-build
 cd taglib-build
-cmake ../taglib-1.11.1
+if [ "$CXX" == "clang++" ]; then
+	cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++" ../taglib-1.11.1
+else
+	cmake ../taglib-1.11.1
+fi
 make && sudo make install
