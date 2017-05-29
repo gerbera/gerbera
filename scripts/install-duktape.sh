c9e9fb319adf72b4dbcd95fecd686dac9a2c8549
Ian Whyman <thev00d00@gentoo.org>
2017-05-29 12:19:46 +0100

    CI: Install duktape

---
+++ scripts/install-duktape.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+wget http://duktape.org/duktape-2.1.0.tar.xz
+tar -xZvf duktape-2.1.0.tar.xz
+cd duktape-2.1.0
+make -f Makefile.sharedlibrary && sudo make -f Makefile.sharedlibrary install
