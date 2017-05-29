6ccbb63ae2cb108af46ea9f27b144cef019f44e3
Ian Whyman <thev00d00@gentoo.org>
2017-05-29 13:09:30 +0100

    CI: Bump libupnp to 1.8.1, use --enable-reuseaddr

--- scripts/install-pupnp18.sh
+++ scripts/install-pupnp18.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-wget https://github.com/mrjimenez/pupnp/archive/release-1.8.0.tar.gz -O pupnp-1.8.0.tgz
-tar -xzvf pupnp-1.8.0.tgz
-cd pupnp-release-1.8.0
-./bootstrap && ./configure --prefix=/usr --enable-ipv6 && make && sudo make install
+wget https://github.com/mrjimenez/pupnp/archive/release-1.8.1.tar.gz -O pupnp-1.8.1.tgz
+tar -xzvf pupnp-1.8.1.tgz
+cd pupnp-release-1.8.1
+./bootstrap && ./configure --prefix=/usr --enable-ipv6 --enable-reuseaddr && make && sudo make install
