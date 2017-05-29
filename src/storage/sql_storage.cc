abea31172e7841ba0830963423bdc261f8cd7b47
Rick Sayre <whorfin@gmail.com>
2017-05-28 17:05:50 -0700

    Re-enable [properly this time] Virtual Folder album art, so that browsing via "Artists" directly in eg foobar2000 properly returns cover art, while also continuing to work when viewing the filesystem directly

--- src/storage/sql_storage.cc
+++ src/storage/sql_storage.cc
@@ -1236,7 +1236,28 @@ String SQLStorage::findFolderImage(int id, String trackArtBase)
        << "LIKE " << quote(_("folder.jp%"));
     *q << " ) AND ";
     *q << TQ("upnp_class") << '=' << quote(_(UPNP_DEFAULT_CLASS_IMAGE_ITEM)) << " AND ";
+#ifndef ONLY_REAL_FOLDER_ART
+    *q << "( ";
+    *q <<       "( ";
+        // virtual listing via Album, Artist etc
+    *q << TQ("parent_id") << " IN ";
+    *q <<       "(";
+    *q << "SELECT " << TQ("parent_id") << " FROM " << TQ(CDS_OBJECT_TABLE) << " WHERE ";
+    *q << TQ("upnp_class") << '=' << quote(_(UPNP_DEFAULT_CLASS_MUSIC_TRACK)) << " AND ";
+    *q << TQ("id") << " IN ";
+    *q <<               "(";
+    *q << "SELECT " << TQ("ref_id") << " FROM " << TQ(CDS_OBJECT_TABLE) << " WHERE ";
+    *q << TQ("parent_id") << '=' << quote(String::from(id)) << " AND ";
+    *q << TQ("object_type") << '=' << quote(OBJECT_TYPE_ITEM);
+    *q <<               ")";
+    *q <<       ")";
+    *q << "     ) OR ";
+#endif
+        // straightforward folder listing of real filesystem
     *q << TQ("parent_id") << '=' << quote(String::from(id));
+#ifndef ONLY_REAL_FOLDER_ART
+    *q << ")";
+#endif
     *q << " ORDER BY " << TQ("dc_title") << " DESC";
 
     //log_debug("findFolderImage %d, %s\n", id, q->c_str());
