e7683ee6e017576902b58e93a0a47deb1d46cd54
Ian Whyman <thev00d00@gentoo.org>
2017-07-30 13:31:43 +0100

    Revert "Dont log the config file XML in debug mode"
    This reverts commit d845ecbfcbc4d6420bf90c4643adba971c63d771.
    
    Thanks to Michael-O for his comments.

--- src/config_manager.cc
+++ src/config_manager.cc
@@ -2083,8 +2083,10 @@ void ConfigManager::validate(String serverhome)
 #endif
 
     log_info("Configuration check succeeded.\n");
+
     //root->indent();
-    //log_debug("Config file dump after validation: \n%s\n", rootDoc->print().c_str());
+
+    log_debug("Config file dump after validation: \n%s\n", rootDoc->print().c_str());
 }
 
 void ConfigManager::prepare_udn()
