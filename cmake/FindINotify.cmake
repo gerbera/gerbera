9edfb3c80fd8ec7613bbc9162e7e7eaa6ef851a7
Ian Whyman <thev00d00@gentoo.org>
2017-09-22 21:15:05 +0100

    CMake: Rewrite FindINotify
    It makes a lot more sense now.

--- cmake/FindINotify.cmake
+++ cmake/FindINotify.cmake
@@ -1,21 +1,26 @@
-# (c) 2014 Copyright ownCloud, Inc.
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING* file.
-
 # This module defines
 #  INOTIFY_INCLUDE_DIR, where to find inotify.h, etc.
 #  INOTIFY_FOUND, If false, do not try to use inotify.
-# also defined, but not for general use are
-#  LIBINOTIFY_LIBRARY, where to find the libinotify library (BSD).
+#  INOTIFY_LIBRARY, where to find the libinotify library (BSD).
 
-find_path(INOTIFY_INCLUDE_DIR sys/inotify.h 
+message(STATUS "Looking for native inotify")
+
+find_path(INOTIFY_INCLUDE_DIR sys/inotify.h
     HINTS /usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
 
-find_library(LIBINOTIFY_LIBRARY inotify)
-
-# all listed variables are TRUE
-# handle the QUIETLY and REQUIRED arguments and set INOTIFY_FOUND to TRUE if
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(INOTIFY DEFAULT_MSG LIBINOTIFY_LIBRARY)
+if (INOTIFY_INCLUDE_DIR AND CMAKE_SYSTEM_NAME MATCHES "Linux")
+    message(STATUS "Looking for native inotify - found")
+    SET(INOTIFY_FOUND TRUE)
+    SET(INOTIFY_LIBRARY "")
+else()
+    message(STATUS "Looking for native inotify - not found")
+    message(STATUS "Looking for libinotify")
+    find_library(INOTIFY_LIBRARY inotify)
+    if (INOTIFY_LIBRARY)
+        message(STATUS "Looking for libinotify - found")
+    endif()
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(INOTIFY DEFAULT_MSG INOTIFY_LIBRARY INOTIFY_INCLUDE_DIR)
+endif()
 
 mark_as_advanced(INOTIFY_LIBRARY INOTIFY_INCLUDE_DIR)
