02cf7ffccbc9d531552a91cd8ba0e08e53c12567
Ian Whyman <thev00d00@gentoo.org>
2017-09-26 20:41:32 +0100

    CMake: tidy up systemd stuff

---
+++ cmake/FindSystemd.cmake
@@ -0,0 +1,20 @@
+#
+# Find systemd using PkgConfig
+#
+# Defines:
+# SYSTEMD_FOUND: if systemd was found
+# SYSTEMD_UNIT_DIR: path to install system unit files
+
+include(FindPackageHandleStandardArgs)
+include(FindPkgConfig)
+
+pkg_check_modules(SYSTEMD systemd QUIET)
+
+if (SYSTEMD_FOUND)
+    execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE}
+        --variable=systemdsystemunitdir systemd
+        OUTPUT_VARIABLE SYSTEMD_SERVICES_INSTALL_DIR)
+    string(REGEX REPLACE "[ \t\n]+" "" SYSTEMD_UNIT_DIR "${SYSTEMD_SERVICES_INSTALL_DIR}")
+endif()
+
+find_package_handle_standard_args(Systemd DEFAULT_MSG SYSTEMD_FOUND)
