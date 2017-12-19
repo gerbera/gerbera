2b68009f5ceb1134e4c2218ef7b813f710316930
Ian Whyman <thev00d00@gentoo.org>
2017-12-19 20:42:49 +0000

    Bump install-pupnp18.sh to 1.8.3

--- scripts/install-pupnp18.sh
+++ scripts/install-pupnp18.sh
@@ -10,7 +10,7 @@ else
    extraFlags="--prefix=/usr"
 fi
 
-wget https://github.com/mrjimenez/pupnp/archive/release-1.8.2.tar.gz -O pupnp-1.8.2.tgz
-tar -xzvf pupnp-1.8.2.tgz
-cd pupnp-release-1.8.2
+wget https://github.com/mrjimenez/pupnp/archive/release-1.8.3.tar.gz -O pupnp-1.8.3.tgz
+tar -xzvf pupnp-1.8.3.tgz
+cd pupnp-release-1.8.3
 ./bootstrap && ./configure $extraFlags --enable-ipv6 --enable-reuseaddr && make && sudo make install
