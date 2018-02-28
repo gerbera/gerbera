8801ffea111bfbf88c588ce6a9c7689ca5024400
astylos <16252944+astylos@users.noreply.github.com>
2018-02-28 21:51:18 +0000

    copyright notice

--- src/search_handler.cc
+++ src/search_handler.cc
@@ -1,3 +1,24 @@
+/*GRB*
+  Gerbera - https://gerbera.io/
+
+  search_handler.cc - this file is part of Gerbera.
+
+  Copyright (C) 2018 Gerbera Contributors
+
+  Gerbera is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2
+  as published by the Free Software Foundation.
+
+  Gerbera is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Gerbera.  If not, see <http://www.gnu.org/licenses/>.
+
+  $Id$
+*/
 #include "search_handler.h"
 #include "config_manager.h"
 #include "storage.h"
--- src/search_handler.h
+++ src/search_handler.h
@@ -1,3 +1,25 @@
+/*GRB*
+  Gerbera - https://gerbera.io/
+
+  search_handler.h - this file is part of Gerbera.
+
+  Copyright (C) 2018 Gerbera Contributors
+
+  Gerbera is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2
+  as published by the Free Software Foundation.
+
+  Gerbera is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Gerbera.  If not, see <http://www.gnu.org/licenses/>.
+
+  $Id$
+*/
+
 /// \file search_handler.h
 /// \brief Definitions of classes supporting implementation of UPnP search
 /// Implements functionality defined in standardizeddcps\MediaServer_4 and MediaRenderer_3\UPnP-av-ContentDirectory-v4-Service-20150630.pdf
