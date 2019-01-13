cee450a7de2eedef0a12c5e570da154c130cdc35
Ian Whyman <thev00d00@gentoo.org>
2019-01-13 15:14:08 +0000

    Docs: Add some custom CSS

--- doc/conf.py
+++ doc/conf.py
@@ -71,7 +71,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -164,5 +164,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
+def setup(app):
+    app.add_stylesheet('css/custom.css')
