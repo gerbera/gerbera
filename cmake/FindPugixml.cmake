c244006aa04ab2e4c5f3e7003ca727e05440238d
pamapa <pamapa@users.noreply.github.com>
2020-01-11 15:00:07 +0100

    part of #567: add pugixml

---
+++ cmake/FindPugixml.cmake
@@ -0,0 +1,27 @@
+# - Try to find libpugixml
+# Once done this will define
+#
+#  PUGIXML_FOUND - system has libpugixml
+#  PUGIXML_INCLUDE_DIRS - the libpugixml include directory
+#  PUGIXML_LIBRARIES - Link these to use PUGIXML
+#
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+find_package(PkgConfig QUIET)
+
+pkg_check_modules(PC_EBM QUIET libpugixml)
+find_path(PUGIXML_INCLUDE_DIR pugixml.hpp
+    HINTS ${PC_PUGIXML_INCLUDEDIR} ${PC_PUGIXML_INCLUDE_DIRS})
+FIND_LIBRARY(PUGIXML_LIBRARY pugixml
+    HINTS ${PC_PUGIXML_LIBDIR} ${PC_PUGIXML_LIBRARY_DIRS})
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PUGIXML
+    REQUIRED_VARS PUGIXML_LIBRARY PUGIXML_INCLUDE_DIR)
+if (PUGIXML_FOUND)
+    set (PUGIXML_LIBRARIES ${PUGIXML_LIBRARY} ${PC_PUGIXML_LIBRARIES})
+    set (PUGIXML_INCLUDE_DIRS ${PUGIXML_INCLUDE_DIR} )
+endif ()
+MARK_AS_ADVANCED(
+    PUGIXML_INCLUDE_DIR
+    PUGIXML_LIBRARY
+)
