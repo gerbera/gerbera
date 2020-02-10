e5bfac0f80c90448211a0c2ab304da04ca874414
Ian Whyman <ian@gerbera.io>
2020-02-10 21:04:30 +0000

    scripts: install-pupnp.sh - backport C++ fix

--- scripts/install-pupnp.sh
+++ scripts/install-pupnp.sh
@@ -15,8 +15,14 @@ else
 fi
 
 wget https://github.com/mrjimenez/pupnp/archive/release-$VERSION.tar.gz -O pupnp-$VERSION.tgz
+
+# Backport C++ fix
+wget https://github.com/pupnp/pupnp/commit/5a8e93f1a57cce5cead5c8c566a75f7c7c294c97.patch -O cxx.patch
+
 tar -xzvf pupnp-$VERSION.tgz
 cd pupnp-release-$VERSION
+# Backport C++ fix
+patch -p1 < ../cxx.patch
 ./bootstrap && \
 ./configure $extraFlags --enable-ipv6 --enable-reuseaddr && \
 make -j$(nproc) && \
