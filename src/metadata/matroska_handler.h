8efd19a8dbf9a57424f24eb3af94b4f51aed2f82
pamapa <pamapa@users.noreply.github.com>
2020-03-28 17:55:36 +0100

    adapt license of file

--- src/metadata/matroska_handler.h
+++ src/metadata/matroska_handler.h
@@ -1,24 +1,23 @@
-/*MT*
+/*GRB*
+
+    Gerbera - https://gerbera.io/
     
-    MediaTomb - http://www.mediatomb.cc/
+    matroska_handler.h - this file is part of Gerbera.
     
-    matroska_handler.h - this file is part of MediaTomb.
+    Copyright (C) 2019-2020 Gerbera Contributors
     
-    Copyright (C) 2019 Patrick Ammann <pammann@gmx.net>
-    
-    MediaTomb is free software; you can redistribute it and/or modify
+    Gerbera is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2
     as published by the Free Software Foundation.
-    
-    MediaTomb is distributed in the hope that it will be useful,
+
+    Gerbera is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License
-    version 2 along with MediaTomb; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-    
+    along with Gerbera.  If not, see <http://www.gnu.org/licenses/>.
+
     $Id$
 */
 
