5c0353827804ce95146eb757172af15bb40e5d00
Ian Whyman <ian@gerbera.io>
2020-04-11 17:03:04 +0100

    install-pupnp: bump to 1.12.1

--- scripts/install-pupnp.sh
+++ scripts/install-pupnp.sh
@@ -5,7 +5,7 @@ if ! [ "$(id -u)" = 0 ]; then
 fi
 set -ex
 
-VERSION="1.12.0"
+VERSION="1.12.1"
 
 unamestr=$(uname)
 if [ "$unamestr" == 'FreeBSD' ]; then
@@ -16,13 +16,8 @@ fi
 
 wget https://github.com/mrjimenez/pupnp/archive/release-$VERSION.tar.gz -O pupnp-$VERSION.tgz
 
-# Backport C++ fix
-wget https://github.com/pupnp/pupnp/commit/5a8e93f1a57cce5cead5c8c566a75f7c7c294c97.patch -O cxx.patch
-
 tar -xzvf pupnp-$VERSION.tgz
 cd pupnp-release-$VERSION
-# Backport C++ fix
-patch -p1 < ../cxx.patch
 ./bootstrap && \
 ./configure $extraFlags --enable-ipv6 --enable-reuseaddr && \
 make -j$(nproc) && \
