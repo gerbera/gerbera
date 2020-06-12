b9e5d227919b6e37d04298bf7574462f1b81b118
Karlchen <k_straussberger@netzland.net>
2020-06-12 23:56:52 +0200

    Lost some code

--- src/web/directories.cc
+++ src/web/directories.cc
@@ -99,7 +99,7 @@ void web::directories::process()
             continue;
         if (std::find(excludes_fullpath.begin(), excludes_fullpath.end(), filepath) != excludes_fullpath.end())
             continue;
-        if ::find(excludes_dirname.begin(), excludes_dirname.end(), filepath.filename()) != excludes_dirname.end()
+        if (std::find(excludes_dirname.begin(), excludes_dirname.end(), filepath.filename()) != excludes_dirname.end()
             || (exclude_config_dirs && startswith(filepath.filename(), ".")))
             continue;
 
