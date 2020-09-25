10ccbdd0a27a5cd9523797589206aa1b82699c7d
Rosen Penev <rosenp@gmail.com>
2020-09-24 20:04:49 -0700

    clang-tidy: add several deleted operators
    Found with cppcoreguidelines-special-member-functions
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/metadata/matroska_handler.cc
+++ src/metadata/matroska_handler.cc
@@ -70,6 +70,11 @@ public:
         close();
     }
 
+    file_io_callback(const file_io_callback&) = delete;
+    file_io_callback& operator=(const file_io_callback&& rhs) = delete;
+    file_io_callback& operator=(const file_io_callback&) = delete;
+    file_io_callback(const file_io_callback&& rhs) = delete;
+
     uint32 read(void* buffer, size_t size) override
     {
         assert(file != nullptr);
