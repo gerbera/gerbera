a87ef2cd127eb5b59d6bec3fa0ea4ef2475ff7db
Ian Whyman <ian@gerbera.io>
2020-12-16 15:20:54 +0000

    scripts: Improve install-googletest.sh

--- scripts/install-googletest.sh
+++ scripts/install-googletest.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail
+
+cleanup() {
+  rm -rf "${script_dir}/build"
+  rm "${script_dir}/gtest.tgz"
+  rm -rf "${script_dir}/gtest"
+}
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
 if ! [ "$(id -u)" = 0 ]; then
     echo "Please run this script with superuser access!"
     exit 1
@@ -6,30 +17,24 @@ fi
 ##
 ## Install the latest version of GoogleTest
 ##
-pwd=$(pwd)
-unamestr=$(uname)
-if [ -f "$pwd/gtest.tgz" ]; then rm -f "$pwd/gtest.tgz"; fi
-if [ -d "$pwd/gtest" ]; then rm -Rf "$pwd/gtest"; fi
 
 ## Download GoogleTest from GitHub
-wget https://github.com/google/googletest/archive/release-1.10.0.tar.gz -O gtest.tgz
-mkdir gtest || exit 1
-tar -xf gtest.tgz --strip 1 -C gtest || exit 1
-cd gtest || exit 1
+mkdir "${script_dir}/gtest"
+wget https://github.com/google/googletest/archive/release-1.10.0.tar.gz -O "${script_dir}/gtest.tgz"
+tar -xf "${script_dir}/gtest.tgz" --strip 1 -C "${script_dir}/gtest"
 
 ## Build GoogleTest using CMake
-mkdir build && cd build || exit 1
-cmake -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -std=c++17" ../ || exit 1
+mkdir "${script_dir}/build"
+cd "${script_dir}/build"
+cmake -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS:-} -std=c++17" ../gtest
 
 if command -v nproc >/dev/null 2>&1; then
-    make -j$(nproc)
+    make "-j$(nproc)"
 else
     make
 fi
 make install
 
-if [ "$unamestr" != 'Darwin' ]; then
+if [ "$(uname)" != 'Darwin' ]; then
     ldconfig
 fi
-
-cd "$pwd" || exit 1
