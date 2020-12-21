c838f24636da3a607fc315e0164af806698d94a9
Ian Whyman <ian@gerbera.io>
2020-12-21 19:30:58 +0000

    install-duktape: tidy up after yourself

--- scripts/install-duktape.sh
+++ scripts/install-duktape.sh
@@ -1,26 +1,37 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+DUK_VER="2.6.0"
+UNAME=$(uname)
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  rm "${script_dir}/duktape.tar.xz"
+  rm -rf "${script_dir}/duk-src"
+}
+trap cleanup SIGINT SIGTERM ERR EXIT
+
 if [ "$(id -u)" != 0 ]; then
     echo "Please run this script with superuser access!"
     exit 1
 fi
-set -ex
 
-DUK_VER="2.5.0"
+wget http://duktape.org/duktape-$DUK_VER.tar.xz -O "${script_dir}/duktape.tar.xz"
+mkdir "${script_dir}/duk-src"
+tar -xf "${script_dir}/duktape.tar.xz" --strip-components=1 -C "${script_dir}/duk-src"
 
-makeCMD="make"
-unamestr=$(uname)
-if [ "$unamestr" = 'FreeBSD' ]; then
-   makeCMD="gmake"
-fi
+cd "${script_dir}/duk-src"
 
-wget http://duktape.org/duktape-$DUK_VER.tar.xz
-tar -xJvf duktape-$DUK_VER.tar.xz
-cd duktape-$DUK_VER
-
-if [ "$unamestr" = 'Darwin' ]; then
+if [ "${UNAME}" = 'Darwin' ]; then
   # Patch Makefile to install on macOS
   # macOS does not support -soname, replace with -install_name
   sed -i -e 's/-soname/-install_name/g' Makefile.sharedlibrary
+fi
+
+makeCMD="make"
+if [ "${UNAME}" = 'FreeBSD' ]; then
+   makeCMD="gmake"
 fi
 
 $makeCMD -f Makefile.sharedlibrary && $makeCMD -f Makefile.sharedlibrary install || exit 1
