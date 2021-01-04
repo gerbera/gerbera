ef593dd1d562b4a67e2f134a5ed376b6889fb922
Ian Whyman <ian@gerbera.io>
2021-01-04 15:14:33 +0000

    Happy New Year

--- scripts/js/import_structured.js
+++ scripts/js/import_structured.js
@@ -3,7 +3,7 @@
 
   import_structured.js - this file is part of Gerbera.
 
-  Copyright (C) 2018-2020 Gerbera Contributors
+  Copyright (C) 2018-2021 Gerbera Contributors
 
   Gerbera is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 2
--- src/metadata/matroska_handler.h
+++ src/metadata/matroska_handler.h
@@ -4,7 +4,7 @@
     
     matroska_handler.h - this file is part of Gerbera.
     
-    Copyright (C) 2019-2020 Gerbera Contributors
+    Copyright (C) 2019-2021 Gerbera Contributors
     
     Gerbera is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2
