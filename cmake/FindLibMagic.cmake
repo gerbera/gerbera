082da206d0931b257ceec52d24bd77526595e779
Ian Whyman <ian@gerbera.io>
2021-01-08 09:49:26 +0000

    CMake: Refactor FindLibMagic
    - Lowercase cmake functions
    - Internal libs are now MAGIC_
    - Correctly check libs and inc dir are found
    - Fix whitespace

--- cmake/FindLibMagic.cmake
+++ cmake/FindLibMagic.cmake
@@ -1,21 +1,21 @@
-INCLUDE (FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 
 find_package(PkgConfig QUIET)
 
-pkg_check_modules(PC_MAGIC QUIET libmagic)
+pkg_search_module(PC_MAGIC QUIET libmagic)
 
-FIND_PATH(LibMagic_INCLUDE_DIR magic.h
+find_path(MAGIC_INCLUDE_DIR magic.h
     HINTS ${PC_MAGIC_INCLUDEDIR} ${PC_MAGIC_INCLUDE_DIRS})
-FIND_LIBRARY(LibMagic_LIBRARIES NAMES magic
+find_library(MAGIC_LIBRARY NAMES magic
     HINTS ${PC_MAGIC_LIBDIR} ${PC_MAGIC_LIBRARY_DIRS})
 
 # handle the QUIETLY and REQUIRED arguments and set MAGIC_FOUND to TRUE
-find_package_handle_standard_args(LibMagic DEFAULT_MSG LibMagic_LIBRARIES)
+find_package_handle_standard_args(LibMagic DEFAULT_MSG MAGIC_LIBRARY MAGIC_LIBRARY)
 
 if (LibMagic_FOUND)
-    set (LibMagic_LIBRARIES ${MAGIC_LIBRARY} ${PC_MAGIC_LIBRARIES})
-    set (LibMagic_INCLUDE_DIRS ${MAGIC_INCLUDE_DIR} )
-endif ()
+    set(LibMagic_LIBRARIES ${MAGIC_LIBRARY})
+    set(LibMagic_INCLUDE_DIRS ${MAGIC_INCLUDE_DIR})
+endif()
 
 MARK_AS_ADVANCED(
         LibMagic_LIBRARIES
