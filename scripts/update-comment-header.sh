db9395d64f16a2374527608c96d255ab07043c8b
Ian Whyman <ian@gerbera.io>
2021-01-24 20:49:53 +0000

    Scripts: delete outdated scripts

--- scripts/update-comment-header.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find web webnew src tombupnp config scripts/js | grep -E '\.(cc?|h|html?|js|css|xsd)$' | xargs perl scripts/update-comment-header.pl
-perl scripts/update-comment-header.pl configure.ac
