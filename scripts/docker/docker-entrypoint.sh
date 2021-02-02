6e98805242c8b3f7cc08d2200b9e3a36163b6755
Ian Whyman <ian@gerbera.io>
2021-02-02 06:13:24 +0000

    Docker: Non root support, scan /content by default (#1190)
    Adds entrypoint and su-exec to drop down to a gerbera user if run as
    root.
    /var/run/gerbera is world writable to make arbitary users work
    correctly.
    Updated cofig to autoscan /content instead of /root
    
    Fix to stop cmake complaining loudly about mariadb-connector-c not
    existing.

---
+++ scripts/docker/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Generate a config file with home set
+gerbera --create-config --home /var/run/gerbera > /var/run/gerbera/config.xml
+
+# Automatically scan /content with inotify (for a volume mount)
+sed 's/<import hidden-files="no">/<import hidden-files="no">\n\
+  <autoscan use-inotify="yes">\n\
+  <directory location="\/content" mode="inotify" \
+  recursive="yes" hidden-files="no"\/>\n\
+  <\/autoscan>/' -i /var/run/gerbera/config.xml
+
+# If we are root, chown home and drop privs
+if [ "$1" = 'gerbera' -a "$(id -u)" = '0' ]; then
+    chown -R gerbera /var/run/gerbera
+    exec su-exec gerbera "$@"
+else
+    # Otherwise run as the provided user
+    exec "$@"
+fi
