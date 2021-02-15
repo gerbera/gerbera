8c237cf858b6bd7e9dcb4bb108f56a30f9b4e2fe
Karlchen <k_straussberger@netzland.net>
2021-02-15 10:11:26 +0100

    Update tests

--- test/scripting/mock/common_script_mock.h
+++ test/scripting/mock/common_script_mock.h
@@ -15,10 +15,11 @@ public:
     virtual ~CommonScriptInterface() { }
     virtual duk_ret_t getPlaylistType(string type) = 0;
     virtual duk_ret_t print(string text) = 0;
+    virtual duk_ret_t addContainerTree(vector<string> tree) = 0;
     virtual duk_ret_t createContainerChain(vector<string> chain) = 0;
     virtual duk_ret_t getLastPath(string path) = 0;
     virtual duk_ret_t readln(string line) = 0;
-    virtual duk_ret_t addCdsObject(map<string, string> item, string playlistContainer, string objectType) = 0;
+    virtual duk_ret_t addCdsObject(map<string, string> item, string containerChain, string objectType) = 0;
     virtual duk_ret_t copyObject(bool isObject) = 0;
     virtual duk_ret_t getCdsObject(string location) = 0;
     virtual duk_ret_t getYear(string year) = 0;
@@ -30,10 +31,11 @@ class CommonScriptMock : public CommonScriptInterface {
 public:
     MOCK_METHOD1(getPlaylistType, duk_ret_t(string type));
     MOCK_METHOD1(print, duk_ret_t(string text));
+    MOCK_METHOD1(addContainerTree, duk_ret_t(vector<string> tree));
     MOCK_METHOD1(createContainerChain, duk_ret_t(vector<string> chain));
     MOCK_METHOD1(getLastPath, duk_ret_t(string path));
     MOCK_METHOD1(readln, duk_ret_t(string line));
-    MOCK_METHOD3(addCdsObject, duk_ret_t(map<string, string> item, string playlistContainer, string objectType));
+    MOCK_METHOD3(addCdsObject, duk_ret_t(map<string, string> item, string containerChain, string objectType));
     MOCK_METHOD1(copyObject, duk_ret_t(bool isObject));
     MOCK_METHOD1(getCdsObject, duk_ret_t(string location));
     MOCK_METHOD1(getYear, duk_ret_t(string year));
