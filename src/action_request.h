d498a488efcbc4a12c92d2c3d1b289a36560b6de
dyseg <bttf87@gmail.com>
2021-03-10 17:56:47 +0000

    expose client quirks from ActionRequest

--- src/action_request.h
+++ src/action_request.h
@@ -97,6 +97,9 @@ public:
     /// \brief Returns the XML representation of the request, that comes to us.
     std::unique_ptr<pugi::xml_document> getRequest() const;
 
+    /// \brief Returns the client quirks
+    std::shared_ptr<Quirks> getQuircks() const;
+
     /// \brief Sets the response (XML created outside as the answer to the request)
     /// \param response XML holding the action response.
     void setResponse(std::unique_ptr<pugi::xml_document>& response);
