b834ee96483c1aef1d31034c64b7c0f18fe7bf66
Karlchen <k_straussberger@netzland.net>
2021-06-04 01:54:03 +0200

    Refactor SearchHandler to use ColumnMapper instead of hard coded texts.

--- src/upnp_cds.cc
+++ src/upnp_cds.cc
@@ -226,7 +226,7 @@ void ContentDirectoryService::doGetSearchCapabilities(const std::unique_ptr<Acti
 
     auto response = UpnpXMLBuilder::createResponse(request->getActionName(), UPNP_DESC_CDS_SERVICE_TYPE);
     auto root = response->document_element();
-    root.append_child("SearchCaps").append_child(pugi::node_pcdata).set_value("dc:title,upnp:class,upnp:artist,upnp:album");
+    root.append_child("SearchCaps").append_child(pugi::node_pcdata).set_value(SQLDatabase::getSearchCapabilities().c_str());
     request->setResponse(std::move(response));
 
     log_debug("end");
