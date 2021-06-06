86f21c64261836732a1ac68bef8c1a28f684eaef
Zoltán Mizsei <zmizsei@extrowerk.com>
2021-06-06 13:57:04 +0200

    Haiku have setgid

--- src/main.cc
+++ src/main.cc
@@ -261,7 +261,7 @@ int main(int argc, char** argv, char** envp)
 // mac os x does this differently, setgid and setuid are basically doing the same
 // as setresuid and setresgid on linux: setting all of real{u,g}id, effective{u,g}id and saved-set{u,g}id
 // Solaroid systems are likewise missing setresgid and setresuid
-#if defined(__APPLE__) || defined(SOLARIS) || defined(__CYGWIN__)
+#if defined(__APPLE__) || defined(SOLARIS) || defined(__CYGWIN__) || defined(__HAIKU__)
             // set group-ids, then add. groups, last user-ids, all need to succeed
             if (0 != setgid(user_id->pw_gid) || 0 != initgroups(user_id->pw_name, user_id->pw_gid) || 0 != setuid(user_id->pw_uid)) {
 #else
