d259162a05ae56878df8dd4f610f38ae45e54898
Rosen Penev <rosenp@gmail.com>
2021-06-05 20:12:59 -0700

    use static_pointer_cast
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/content/content_manager.cc
+++ src/content/content_manager.cc
@@ -1968,7 +1968,7 @@ void CMFetchOnlineContentTask::run()
         return;
     }
     try {
-        std::shared_ptr<GenericTask> t = std::make_shared<TPFetchOnlineContentTask>(content, task_processor, timer, service, layout, cancellable, unscheduled_refresh);
+        auto t = std::static_pointer_cast<GenericTask>(std::make_shared<TPFetchOnlineContentTask>(content, task_processor, timer, service, layout, cancellable, unscheduled_refresh));
         task_processor->addTask(t);
     } catch (const std::runtime_error& ex) {
         log_error("{}", ex.what());
