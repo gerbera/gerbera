f6363173f716d32f0f2a84ae73cf8d5ffb9eaa87
Rosen Penev <rosenp@gmail.com>
2021-06-06 03:39:28 -0700

    test: static conversions
    Found with readability-convert-member-functions-to-static
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- test/scripting/mock/duk_helper.h
+++ test/scripting/mock/duk_helper.h
@@ -19,11 +19,11 @@ public:
 
     // Convert an array at the top of the stack
     // to a c++ vector of strings
-    std::vector<std::string> arrayToVector(duk_context* ctx, duk_idx_t idx);
+    static std::vector<std::string> arrayToVector(duk_context* ctx, duk_idx_t idx);
 
     // Convert an array of objects containing title property at the top of the stack
     // to a c++ vector of strings
-    std::vector<std::string> containerToPath(duk_context* ctx, duk_idx_t idx);
+    static std::vector<std::string> containerToPath(duk_context* ctx, duk_idx_t idx);
 
     // Convert an object at the top of the stack
     // to a map of its properties using the property keys passed into
@@ -31,20 +31,20 @@ public:
     std::map<std::string, std::string> extractValues(duk_context* ctx, const std::vector<std::string>& keys, duk_idx_t idx);
 
     // Adds a new object to the duk_context with meta data
-    void createObject(duk_context* ctx, const std::map<std::string, std::string>& objectProps, const std::map<std::string, std::string>& metaProps);
+    static void createObject(duk_context* ctx, const std::map<std::string, std::string>& objectProps, const std::map<std::string, std::string>& metaProps);
 
 private:
     // Extracts the property value given a complex associative array key
     // Example: key = `meta['dc:title']`
     // Finds *meta* object, then its child property *dc:title*
-    std::tuple<std::string, std::string> extractObjectValues(duk_context* ctx, const std::string& key, duk_idx_t idx);
+    static std::tuple<std::string, std::string> extractObjectValues(duk_context* ctx, const std::string& key, duk_idx_t idx);
 
     // Returns true if the key is representing javascript array notation
     // true = meta['dc:title']
     // false = 'dc:title'
     // false = 'meta'
     // false = 'meta.title' // TODO: should be true...
-    bool isArrayNotation(const std::string& key);
+    static bool isArrayNotation(const std::string& key);
 };
 
 #endif //GERBERA_DUK_HELPER_H
