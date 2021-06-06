5138603a18d46abd4fd8ec34cf00864741d8f147
Zoltán Mizsei <zmizsei@extrowerk.com>
2021-06-06 13:58:31 +0200

    pthread_attr_setinheritsched is not implemented on Haiku yet

--- src/util/thread_runner.h
+++ src/util/thread_runner.h
@@ -177,9 +177,10 @@ protected:
     /// \brief start the thread
     void startThread() override
     {
-#ifndef SOLARIS
+#if !defined(SOLARIS) && !defined(__HAIKU__)
         // default scoping on solaroid systems is in fact PTHREAD_SCOPE_SYSTEM
         // plus, setting PTHREAD_EXPLICIT_SCHED requires elevated priveleges
+		// while Haiku doesn't implements pthread_attr_setinheritsched yet
         if (config->getBoolOption(CFG_THREAD_SCOPE_SYSTEM)) {
             attr = new pthread_attr_t;
             pthread_attr_init(attr);
