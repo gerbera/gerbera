ac2ad1373406f9122cb12814b9f81782c51db719
Rosen Penev <rosenp@gmail.com>
2021-06-13 14:55:01 -0700

    replace const static with static const
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/database/search_handler.cc
+++ src/database/search_handler.cc
@@ -464,7 +464,7 @@ std::string DefaultSQLEmitter::emit(const ASTParenthesis* node, const std::strin
 //    1: property name statement
 //    2: property statement lower case
 //    3: value
-const static std::map<std::string, std::string> logicOperator = {
+static const std::map<std::string, std::string> logicOperator = {
     { "contains", "({2} LIKE LOWER('%{3}%') AND {0} IS NOT NULL)" }, // lower
     { "doesnotcontain", "({2} NOT LIKE LOWER('%{3}%') AND {0} IS NOT NULL)" }, // lower
     { "startswith", "({2} LIKE LOWER('{3}%') AND {0} IS NOT NULL)" }, // lower
