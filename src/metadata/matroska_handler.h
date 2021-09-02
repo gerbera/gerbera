46fab19c94aa5487706c5f4bd345d4f446701600
Rosen Penev <rosenp@gmail.com>
2021-09-02 16:29:31 -0700

    matroska: change pointer to reference
    There's no nullptr here.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/metadata/matroska_handler.h
+++ src/metadata/matroska_handler.h
@@ -53,7 +53,7 @@ private:
     void parseLevel1Element(const std::shared_ptr<CdsItem>& item, LIBEBML_NAMESPACE::EbmlStream& ebml_stream, LIBEBML_NAMESPACE::EbmlElement* el_l1, std::unique_ptr<MemIOHandler>* p_io_handler) const;
     void parseInfo(const std::shared_ptr<CdsItem>& item, EbmlStream& ebml_stream, LIBEBML_NAMESPACE::EbmlMaster* info) const;
     void parseAttachments(const std::shared_ptr<CdsItem>& item, LIBEBML_NAMESPACE::EbmlStream& ebml_stream, LIBEBML_NAMESPACE::EbmlMaster* attachments, std::unique_ptr<MemIOHandler>* io_handler) const;
-    std::string getContentTypeFromByteVector(const LIBMATROSKA_NAMESPACE::KaxFileData* data) const;
+    std::string getContentTypeFromByteVector(const LIBMATROSKA_NAMESPACE::KaxFileData& data) const;
     static void addArtworkResource(const std::shared_ptr<CdsItem>& item, const std::string& art_mimetype);
 };
 
