3fd507ebbb6af8837f190867081776b6e49e544a
SmartSmurf <smartsmurf@shared-files.de>
2021-11-08 12:56:10 +0100

    cross-compiled Gerbera (gcc-11.2 for ARM target) segfaults when opening the configuration tab in web browser. Debugging revealed the it happens inside Web::ConfigLoad::process() This patch fixed it for me.

--- src/web/pages.h
+++ src/web/pages.h
@@ -189,7 +189,7 @@ public:
 class ConfigLoad : public WebRequestHandler {
 protected:
     std::vector<ConfigValue> dbEntries;
-    std::map<std::string, pugi::xml_node*> allItems;
+    std::map<std::string, pugi::xml_node> allItems;
     void createItem(pugi::xml_node& item, const std::string& name, config_option_t id, config_option_t aid, const std::shared_ptr<ConfigSetup>& cs = nullptr);
     template <typename T>
     static void setValue(pugi::xml_node& item, const T& value);
