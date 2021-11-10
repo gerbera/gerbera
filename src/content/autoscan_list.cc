a3d819224b707c52fc35cb05b4251db9d2dcb900
Rosen Penev <rosenp@gmail.com>
2021-11-09 17:33:34 -0800

    use operator < instead of startswith
    Avoids unnecessary conversions.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/content/autoscan_list.cc
+++ src/content/autoscan_list.cc
@@ -157,7 +157,7 @@ std::shared_ptr<AutoscanList> AutoscanList::removeIfSubdir(const fs::path& paren
     for (auto it = list.begin(); it != list.end(); /*++it*/) {
         auto dir = *it;
 
-        if (startswith(dir->getLocation().string(), parent.string())) {
+        if (dir->getLocation() < parent) {
             if (dir->persistent() && !persistent) {
                 ++it;
                 continue;
