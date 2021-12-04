0eb08840cbe438340c5e2a9fd69f65e90bf8974f
Rosen Penev <rosenp@gmail.com>
2021-12-03 19:30:57 -0800

    update findsqlite3
    from version 3.22.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -52,7 +52,7 @@ if(SQLite3_INCLUDE_DIR)
     unset(_ver_line)
 endif()
 
-include(FindPackageHandleStandardArgs)
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(SQLite3
     REQUIRED_VARS SQLite3_INCLUDE_DIR SQLite3_LIBRARY
     VERSION_VAR SQLite3_VERSION)
