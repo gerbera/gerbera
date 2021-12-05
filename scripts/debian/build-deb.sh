9439451f14134d565874412fc909c0f8fe5805d7
Karlchen <k_straussberger@netzland.net>
2021-12-05 15:55:29 +0100

    Fix focal build

--- scripts/debian/build-deb.sh
+++ scripts/debian/build-deb.sh
@@ -19,7 +19,7 @@ function install-gcc {
 
 function install-cmake() {
   echo "::group::Installing CMake"
-  if [[ "$lsb_codename" == "bionic" ]]; then
+  if [[ "$lsb_codename" == "bionic" || "$lsb_codename" == "focal" ]]; then
     sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget -y
     wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
     sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ ${lsb_codename} main"
