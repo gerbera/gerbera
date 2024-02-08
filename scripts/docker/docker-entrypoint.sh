8c97ae6d9c2ec430d76e5ec890659c691689757b
Martin Zuther <code@mzuther.de>
2024-02-08 18:59:37 +0100

    Fix content directory in configuration (Docker)
    - improve readability of regular expressions for `sed`

--- scripts/docker/docker-entrypoint.sh
+++ scripts/docker/docker-entrypoint.sh
@@ -29,15 +29,15 @@ if [ ! -f /var/run/gerbera/config.xml ]; then
   gerbera --create-config --home /var/run/gerbera > /var/run/gerbera/config.xml
 
   # Automatically scan /content with inotify (for a volume mount)
-  sed 's/<import hidden-files="no">/<import hidden-files="no">\n\
+  sed 's|<import hidden-files="no">|<import hidden-files="no">\n\
     <autoscan use-inotify="yes">\n\
-    <directory location="\/content" mode="inotify" \
-    recursive="yes" hidden-files="no"\/>\n\
-    <\/autoscan>/' -i /var/run/gerbera/config.xml
+    <directory location="/mnt/content" mode="inotify" \
+               recursive="yes" hidden-files="no"/>\n\
+    </autoscan>|' -i /var/run/gerbera/config.xml
 
   # Add directory for custom JavaScript scripts
-  sed 's/<\/common>/<\/common>\
-        <custom>\/mnt\/customization\/js<\/custom>/' -i /var/run/gerbera/config.xml
+  sed 's|</common>|</common>\
+        <custom>/mnt/customization/js</custom>|' -i /var/run/gerbera/config.xml
 
   # Allow customization of Gerbera configuration file
   if [ -x /mnt/customization/shell/gerbera_config.sh ]; then
