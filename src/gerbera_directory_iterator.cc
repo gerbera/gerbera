08848e76b55832455aabf9beaa0f4db1959523ce
Ступаков М.С. <stupakov@codlix.ru>
2024-12-21 09:14:38 +0300

    Added required code headers

--- src/gerbera_directory_iterator.cc
+++ src/gerbera_directory_iterator.cc
@@ -1,3 +1,28 @@
+/*GRB*
+
+    Gerbera - https://gerbera.io/
+
+    gerbera_directory_iterator.cc - this file is part of Gerbera.
+
+    Copyright (C) 2024 Gerbera Contributors
+
+    Gerbera is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    Gerbera is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gerbera.  If not, see <http://www.gnu.org/licenses/>.
+
+    $Id$
+*/
+
+/// \file gerbera_directory_iterator.cc
+
 #include "gerbera_directory_iterator.h"
 #include <regex>
 
