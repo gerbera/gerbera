60ee5d698288ca6b134d172522b477d4040e65f0
Karlchen <k_straussberger@netzland.net>
2025-10-26 17:40:59 +0100

    Fix help test

--- test/core/test_server.cc
+++ test/core/test_server.cc
@@ -39,6 +39,7 @@
 
 namespace fs = std::filesystem;
 
+using ::testing::ContainsRegex;
 using ::testing::HasSubstr;
 
 static constexpr auto gitBranch = std::string_view(GIT_BRANCH);
@@ -79,19 +80,18 @@ public:
 
 TEST_F(ServerTest, ServerOutputsHelpInformation)
 {
-    std::string expectedOutput = mockText("fixtures/mock-help.out");
-    std::smatch base_match;
+    std::string expected = mockText("fixtures/mock-help.out");
     auto result = std::vector<std::string>();
     auto patternList = std::vector<std::string> {
         "-h, --help *Print this help and exit",
         "-D, --debug *Enable debugging",
         "-v, --version *Print version info and exit",
         "    --compile-info *Print compile info and exit",
-        "    --create-config [=Section(=All)]",
+        "    --create-config \\[=Section\\(=All\\)\\]",
         " *Print a default config.xml file and exit",
-        "    --create-example-config [=Section(=All)]",
+        "    --create-example-config \\[=Section\\(=All\\)\\]",
         " *Print a example config.xml file and exit",
-        "    --create-advanced-config [=Section(=All)]",
+        "    --create-advanced-config \\[=Section\\(=All\\)\\]",
         " *Print a advanced example config.xml file ",
         " *and exit",
         "    --check-config *Check config.xml file and exit",
@@ -100,19 +100,19 @@ TEST_F(ServerTest, ServerOutputsHelpInformation)
         "    --drop-tables *Drop all database tables and exit",
 #ifdef HAVE_LASTFM
 #ifndef HAVE_LASTFMLIB
-        ".*--init-lastfm.*Get Last\\.FM session key.*",
+        " *--init-lastfm.*Get Last\\.FM session key.*",
 #endif
 #endif
         "-d, --daemon *Daemonize after startup",
         "-u, --user UID *Drop privs to user UID",
         "-P, --pidfile FILE *Write a pidfile to the specified location, ",
         " *e.g. /run/gerbera.pid",
-        "-c, --config DIR *Path to config file",
+        "-c, --config FILE *Path to config file",
         "-f, --cfgdir DIR *Override name of config folder ",
-        " *(.config/gerbera) in home folder",
+        " *\\(.config/gerbera\\) in home folder",
         "-l, --logfile FILE *Set log location",
         "    --rotatelog FILE *Set rotating log location",
-        "    --syslog [=LOG(=USER)] *Log to syslog",
+        "    --syslog \\[=LOG\\(=USER\\)\\] *Log to syslog",
         "    --add-file FILE *Scan a file into the DB on startup, can be ",
         " *specified multiple times",
         "    --set-option OPT=VAL *Set simple config option OPT to value VAL, ",
@@ -123,28 +123,44 @@ TEST_F(ServerTest, ServerOutputsHelpInformation)
         "-e, --interface IF *Interface to bind with",
         "-m, --home DIR *Search this directory for a .config/gerbera ",
         " *folder containing a config file",
-#ifndef HAVE_MAGIC
+#ifdef HAVE_MAGIC
         "    --magic FILE *Set magic FILE",
 #endif
-        "    --import-mode MODE *Activate import MODE ('mt' or 'grb')",
-#ifndef GRBDEBUG
+        "    --import-mode MODE *Activate import MODE \\('mt' or 'grb'\\)",
+#ifdef GRBDEBUG
         "    --debug-mode FACILITY *Set debugging mode to FACILITY.*",
 #endif
     };
-    for (auto&& line : splitString(expectedOutput, '\n', '\0', true)) {
-        bool found = false;
-        for (auto&& pattern : patternList) {
-            if (std::regex_match(line, base_match, std::regex(pattern)))
-                found = true;
-        }
-        if (!found)
-            result.push_back(line);
-    }
-    expectedOutput = fmt::format("{}", fmt::join(result, "\n"));
+
     fs::path cmd = fs::path(CMAKE_BINARY_DIR) / "gerbera --help 2>&1";
     std::string output = exec(cmd.c_str());
 
-    ASSERT_THAT(output.c_str(), HasSubstr(expectedOutput));
+    auto expectedOutput = splitString(expected, '\n', '\0', true);
+    for (auto&& line : expectedOutput) {
+        ASSERT_THAT(output, HasSubstr(line));
+    }
+    for (auto&& pattern : patternList) {
+        ASSERT_THAT(output, ContainsRegex(pattern));
+    }
+
+    patternList.push_back("/home/runner/\\.conan2.*"); // libuuid causes error message under conan :-(
+    for (auto&& line : splitString(output, '\n', '\0', true)) {
+        bool found = false;
+        for (auto&& pattern : patternList) {
+            std::smatch base_match;
+            if (!line.empty() && std::regex_search(line, base_match, std::regex(pattern)))
+                found = true;
+        }
+        if (!found) {
+            for (auto&& out : expectedOutput) {
+                if (line.empty() && out.empty())
+                    found = true;
+                if (out == line)
+                    found = true;
+            }
+        }
+        ASSERT_TRUE(found) << "Extra '" << line << "'";
+    }
 }
 
 TEST_F(ServerTest, ServerOutputsCompileInformationIncludingGit)
