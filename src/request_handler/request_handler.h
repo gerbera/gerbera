772a538e16d9585d3e5dd71cffa9953710f1ae9e
Nicolas PARLANT <nicolas.parlant@parhuet.fr>
2026-06-17 09:29:01 +0200

    add missing headers
    it fixes compilation w/ gcc-17, also tested w/ clang-22
    
    Signed-off-by: Nicolas PARLANT <nicolas.parlant@parhuet.fr>

--- src/request_handler/request_handler.h
+++ src/request_handler/request_handler.h
@@ -36,6 +36,7 @@
 
 #include <map>
 #include <memory>
+#include <string>
 #include <upnp.h>
 
 // forward declaration
--- src/upnp/client_manager.h
+++ src/upnp/client_manager.h
@@ -31,6 +31,7 @@
 #include <chrono>
 #include <memory>
 #include <mutex>
+#include <string>
 #include <vector>
 
 // forward declarations
